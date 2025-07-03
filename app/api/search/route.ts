1f044910cd011a66867fa47995a1f6b52cf71361
anshifmonz <muhammedanshif236@gmail.com>
2025-07-03 09:20:06 +0530

    feat(api/public): add API for fetching public product data

---
+++ app/api/search/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import searchProducts from '@/lib/public/product';
+import { type Product } from '@/types/product';
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get('q');
+  const category = searchParams.get('category');
+  const minPrice = searchParams.get('minPrice');
+  const maxPrice = searchParams.get('maxPrice');
+  const colors = searchParams.get('colors');
+  const tags = searchParams.get('tags');
+
+  const minPriceNumber = minPrice ? parseInt(minPrice) : undefined;
+  const maxPriceNumber = maxPrice ? parseInt(maxPrice) : undefined;
+  const colorsArray = colors ? colors.split(',') : [];
+  const tagsArray = tags ? tags.split(',') : [];
+
+  const products: Product[] = await searchProducts(query, {
+    category,
+    minPrice: minPriceNumber,
+    maxPrice: maxPriceNumber,
+    colors: colorsArray,
+    tags: tagsArray
+  });
+  console.log(products);
+
+  return NextResponse.json(products);
+}
