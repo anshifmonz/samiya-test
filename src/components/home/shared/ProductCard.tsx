87ab6090aae90ab3e271ae428a1c89ec07fbd400
anshifmonz <muhammedanshif236@gmail.com>
2025-07-04 15:51:39 +0530

    style(home): reduce size of ProductCard with previous clamp() and 4:5 aspect ratio

--- src/components/home/shared/ProductCard.tsx
+++ src/components/home/shared/ProductCard.tsx
@@ -6,15 +6,15 @@ const Productcard = ({ product }: { product: SpecialProduct }) => {
   const image = product.images[0];
 
   return (
-    <div className="flex flex-col relative h-[370px] w-[280px] rounded-sm overflow-hidden shadow-sm cursor-pointer">
-      <div className="relative flex-grow w-full h-full overflow-hidden">
+    <div className="relative flex flex-col gap-1 rounded-sm overflow-hidden shadow-sm group cursor-pointer w-[clamp(164px,calc(50vw-30px),458px)] lg:w-[clamp(200px,calc(20vw-16px),243px)]">
+      <div className="relative flex-grow w-full aspect-[4/5] overflow-hidden">
         {image ? (
           <Image
             src={image}
             alt={product.title}
             className="w-full h-full object-cover"
-            width={280}
-            height={370}
+            width={458}
+            height={573}
           />
         ) : (
           <ImageFallback />
@@ -24,9 +24,9 @@ const Productcard = ({ product }: { product: SpecialProduct }) => {
         </div>
       </div>
       <div className="bg-transparent">
-        <h4 className="font-cormorant font-medium text-gray-900 pt-2 text-base leading-tight">{product.title}</h4>
-        <p className="text-md font-inter font-semibold text-gray-900">
-          <span className="text-sm font-light line-through text-gray-500">Rs {product.price}</span> RS {product.price}
+        <h4 className="font-cormorant text-sm md:text-xs text-gray-900 pt-2 leading-tight">{product.title}</h4>
+        <p className="text-sm font-inter font-semibold text-gray-900">
+          <span className="text-[11px] font-light line-through text-gray-500 mr-2">Rs {product.price}</span> Rs {product.price}
         </p>
       </div>
     </div>
