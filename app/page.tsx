6a11d2390e8de2072080fad25268d49cf37aa0ec
anshifmonz <muhammedanshif236@gmail.com>
2025-07-05 18:07:17 +0530

    refactor(home): remove Collections component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Home from '@/components/home';
-import getCollections from '@/lib/collections';
 import getNewArrivals from '@/lib/newarrivals';
 import getSpecials from '@/lib/specials';
 
 export default async function HomePage() {
-  const collections = await getCollections();
   const newArrivals = await getNewArrivals();
   const specials = await getSpecials();
 
   return (
-    <Home collections={collections} newArrivals={newArrivals} specials={specials} />
+    <Home newArrivals={newArrivals} specials={specials} />
   );
 }
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,19 +1,16 @@
 import { type Special, type SpecialProduct } from '@/types/special';
-import { type Collection } from '@/types/collection';
 
 import LandingSection from './LandingSection';
 import CategoryTabs from './CategoryTabs';
-import Collections from './Collections';
 import NewArrivals from './NewArrivals';
 import BudgetSection from './BudgetSelection';
 import SelectionSections from './SpecialSections';
 
-const Home = ({ collections, newArrivals, specials }: { collections: Collection[], newArrivals: SpecialProduct[], specials: Special[] }) => {
+const Home = ({ newArrivals, specials }: { newArrivals: SpecialProduct[], specials: Special[] }) => {
   return (
     <div className="flex flex-col gap-[70px] min-h-screen bg-luxury-white">
       <CategoryTabs />
       <LandingSection />
-      <Collections collections={collections} />
       <NewArrivals newArrivals={newArrivals} />
       <BudgetSection />
       <SelectionSections specials={specials} />
