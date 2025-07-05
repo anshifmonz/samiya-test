20cd4da90f0bca310583b78cbd0ab79dca04d695
anshifmonz <muhammedanshif236@gmail.com>
2025-07-05 22:02:42 +0530

    style(home): update position of ScrollIndicator

--- src/components/home/ScrollIndicator.tsx
+++ src/components/home/ScrollIndicator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ScrollIndicator: React.FC = () => {
   return (
-    <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+    <div className="absolute hidden sm:block sm:bottom-2 lg:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
       <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
         <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
       </div>
