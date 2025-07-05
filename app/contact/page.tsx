0ee767ad291d454844753053512ad123dbe5269d
anshifmonz <muhammedanshif236@gmail.com>
2025-07-05 10:52:58 +0530

    refactor(contact): move ContactSection component to src/components/contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactSection from '@/components/home/ContactSection';
+import ContactSection from 'components/contact/ContactSection';
 
 export default function ContactPage() {
   return (
