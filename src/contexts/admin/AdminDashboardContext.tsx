dc8341c17a58bb76d2c398779d4457bbfeb60293
anshifmonz <muhammedanshif236@gmail.com>
2025-07-28 01:15:03 +0530

    feat(admin/context): create context to provide data of current admin to all AdminTabs

---
+++ src/contexts/admin/AdminDashboardContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from 'react';
+import { useCurrentAdmin as Admin } from 'hooks/admin/useCurrentAdmin';
+
+interface AdminDashboardContextType {
+  admin: any;
+  isLoading: boolean;
+  error: string | null;
+  isSuperAdmin: boolean;
+}
+
+const AdminDashboardContext = createContext<AdminDashboardContextType | undefined>(undefined);
+
+export const AdminDashboardProvider = ({ children }: { children: React.ReactNode }) => {
+  const { admin, isLoading, error, isSuperAdmin } = Admin();
+  return (
+    <AdminDashboardContext.Provider value={{ admin, isLoading, error, isSuperAdmin }}>
+      {children}
+    </AdminDashboardContext.Provider>
+  );
+};
+
+export const useCurrentAdmin = () => {
+  const context = useContext(AdminDashboardContext);
+  if (!context) throw new Error('useAdminDashboardContext must be used within an AdminDashboardProvider');
+  return context;
+};
