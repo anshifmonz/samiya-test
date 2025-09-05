23187ac397c8829e8d2c8e91d6a631107caf9000
anshifmonz <muhammedanshif236@gmail.com>
2025-09-05 13:51:35 +0530

    feat(api/payment/refund): add post-refund handling logic

--- src/lib/utils/payment/cashfree.refund.ts
+++ src/lib/utils/payment/cashfree.refund.ts
@@ -1,6 +1,43 @@
+import retry from 'utils/retry';
 import { RefundEntity } from 'cashfree-pg';
+import { supabaseAdmin } from 'lib/supabase';
 import { getCashfreeInstance } from './cashfree.config';
 import { ApiResponse, err, ok } from 'utils/api/response';
+
+const postRefund = async (orderId: string, refundId: string) => {
+  const { error } = await retry(async () => {
+    return supabaseAdmin
+      .from('payments')
+      .update({
+        status: 'refunded',
+        refund_id: refundId
+      })
+      .eq('order_id', orderId);
+  }, 5);
+  if (!error) return;
+
+  const { error: statusUpdateError } = await retry(async () => {
+    return supabaseAdmin
+      .from('payment_status_update')
+      .insert({
+        order_id: orderId,
+        status: 'refunded'
+      });
+  }, 5);
+  if (statusUpdateError) console.error('payment_status_update: ' + orderId)
+}
+
+const pendingRefunds = async (orderId: string, totalAmount: number) => {
+  const { error } = await retry(async () => {
+    return supabaseAdmin
+      .from('pending_refunds')
+      .insert({
+        order_id: orderId,
+        total_amout: totalAmount
+      });
+  }, 5);
+  if (error) console.error('pending_refunds: ' + orderId + ' ' + totalAmount)
+}
 
 export const createCashfreeRefund = async (
   orderId: string,
@@ -15,8 +52,10 @@ export const createCashfreeRefund = async (
       refund_id: refundId,
       refund_note: reason || ''
     });
+    postRefund(orderId, refundId)
     return ok(null);
   } catch (_) {
+    pendingRefunds(orderId, amount)
     return err('Something went wrong while creating refund');
   }
 };
