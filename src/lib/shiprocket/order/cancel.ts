44f59896567384a2aac4d6b2101d1623aef92d3f
anshifmonz <muhammedanshif236@gmail.com>
2025-09-05 13:50:40 +0530

    feat(api/order/cancel): add post-cancellation handling logic

--- src/lib/shiprocket/order/cancel.ts
+++ src/lib/shiprocket/order/cancel.ts
@@ -6,6 +6,37 @@ import { createCashfreeRefund } from 'utils/payment/cashfree.refund';
 
 const SR_BASE = process.env.SHIPROCKET_BASE_URL || 'https://apiv2.shiprocket.in/v1/external';
 
+async function postCancellation(localOrderId: string) {
+  const { data: orderData, error: orderError } = await retry(async () => {
+    return supabaseAdmin
+      .from('orders')
+      .update({ status: 'cancelled' })
+      .eq('id', localOrderId)
+      .select('total_amount')
+      .single();
+  });
+
+  if (orderError) {
+    const { error } = await retry(async () => {
+      return supabaseAdmin
+        .from('order_status_update')
+        .insert({
+          order_id: localOrderId,
+          status: 'cancelled'
+        });
+    }, 5);
+    if (error) console.error('order_status_update: ' + localOrderId)
+    return;
+  }
+
+  createCashfreeRefund(
+    localOrderId,
+    localOrderId,
+    orderData.total_amount,
+    "Customer don't need this product anymore."
+  );
+}
+
 export async function SRCancelOrder(
   token: string,
   srOrderId: number,
@@ -23,42 +54,6 @@ export async function SRCancelOrder(
     });
     if (error) return err('Failed to cancel order');
   }
-
-  const { data: orderData, error: orderError } = await retry(async () => {
-    return supabaseAdmin
-      .from('orders')
-      .update({ status: 'cancelled' })
-      .eq('id', localOrderId)
-      .select('total_amount')
-      .single();
-  });
-
-  if (orderError) {
-    retry(async () => {
-      return supabaseAdmin
-        .from('pending_order_cancellation')
-        .insert({ order_id: localOrderId });
-    }, 5);
-    return ok(null)
-  }
-
-  const { error: refundError } = await createCashfreeRefund(
-    localOrderId,
-    localOrderId,
-    orderData.total_amount,
-    "Customer don't need this product anymore."
-  );
-
-  if (refundError) {
-    retry(async () => {
-      return supabaseAdmin
-        .from('pending_refunds')
-        .insert({
-          order_id: localOrderId,
-          total_amout: orderData.total_amount
-        });
-    }, 5);
-  }
-
+  postCancellation(localOrderId);
   return ok(null);
 }
