0ed567b42e7108e62c7c59f1bd5c1a75dc7b5934
anshifmonz <muhammedanshif236@gmail.com>
2025-09-23 14:22:38 +0530

    feat(components/navbar): add user dropdown to navbar

--- src/components/shared/navbar/Logo.tsx
+++ src/components/shared/navbar/Logo.tsx
@@ -6,12 +6,13 @@ interface LogoProps {
     logo: string;
     logoSubtext: string;
   };
+  className?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ variant, textStyles }) => {
+const Logo: React.FC<LogoProps> = ({ variant, textStyles, className }) => {
   if (variant === 'desktop') {
     return (
-      <div className="hidden md:block">
+      <div className={`hidden md:block ${className}`}>
         <Link href="/" className={`${textStyles.logo}`}>
           <span>Samiya</span>
           <span className={`${textStyles.logoSubtext}`}>
@@ -23,7 +24,7 @@ const Logo: React.FC<LogoProps> = ({ variant, textStyles }) => {
   }
 
   return (
-    <div className="md:hidden">
+    <div className={`md:hidden ${className}`}>
       <Link href="/" className={`${textStyles.logo}`}>
         <span>Samiya</span>
         <span className={`${textStyles.logoSubtext}`}>
