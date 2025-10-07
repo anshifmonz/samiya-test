37d6c6e3b1dbe79b48492e80a8dfc1aeaba24cc7
anshifmonz <muhammedanshif236@gmail.com>
2025-10-07 21:42:38 +0530

    fix(firebase/init): fix to initialize only once and App Check is only initialized in the browser

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,11 +1,23 @@
 import { getAuth } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebaseConfig';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
-const app = initializeApp(firebaseConfig);
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_FIREBASE_SITE_KEY),
-  isTokenAutoRefreshEnabled: true
-});
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+if (typeof window !== 'undefined') {
+  if (process.env.NEXT_PUBLIC_FIREBASE_SITE_KEY) {
+    try {
+      initializeAppCheck(app, {
+        provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_FIREBASE_SITE_KEY),
+        isTokenAutoRefreshEnabled: true
+      });
+    } catch (error) {
+      console.error('Failed to initialize Firebase App Check:', error);
+    }
+  } else {
+    console.warn('NEXT_PUBLIC_FIREBASE_SITE_KEY is not set. Firebase App Check is disabled.');
+  }
+}
+
 export const auth = getAuth(app);
