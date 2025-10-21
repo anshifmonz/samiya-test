b878be9f2a6cb7b04079cc22e073842c910e9a01
anshifmonz <muhammedanshif236@gmail.com>
2025-10-21 16:55:33 +0530

    feat(ui/badge): include success, info and warning variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,6 +15,12 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        warning:
+          "border-transparent bg-yellow-500 text-white",
+        info:
+          "border-transparent bg-blue-500 text-white",
+        success:
+          "border-transparent bg-green-500 text-white",
       },
     },
     defaultVariants: {
