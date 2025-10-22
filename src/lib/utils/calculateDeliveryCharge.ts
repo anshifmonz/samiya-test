57cc1be175363ebdb1c358118f5f6f60a00e4ab6
anshifmonz <muhammedanshif236@gmail.com>
2025-10-22 08:42:48 +0530

    delivery charge test

--- src/lib/utils/calculateDeliveryCharge.ts
+++ src/lib/utils/calculateDeliveryCharge.ts
@@ -1,2 +1,1 @@
-export const calculateDeliveryCharge = (subtotal: number): number =>
-  subtotal === 0 ? 0 : subtotal < 1000 ? 40 : 0;
+export const calculateDeliveryCharge = (subtotal: number): number => 0;
